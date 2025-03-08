205f86933b34e5301738b4523b6fb8d7c5bf3470
P N <prathmeshnaik.1202@gmail.com>
2025-03-08 16:22:32 +0000

    comments

--- k8s-manifests/bookstore-setup.sh
+++ k8s-manifests/bookstore-setup.sh
@@ -1,8 +1,17 @@
+# create namespace for the bookstore app
 kubectl apply -f bookstore-ns.yml
+
+# add docker credentials
 ./docker-creds.sh
+
+# config maps and secrets
 kubectl apply -f configmap.yml
 kubectl apply -f secrets.yml
+
+# deploy postgres and redis databases
 kubectl apply -f postgres.yml
 kubectl apply -f redis.yml
+
+# deploy the catalog and cart services
 kubectl apply -f catalog-service.yml
 kubectl apply -f cart-service.yml
