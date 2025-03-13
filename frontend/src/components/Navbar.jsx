85370bf7837053dc430ac0a4d36b410594ec6f6b
P N <prathmeshnaik.1202@gmail.com>
2025-03-13 12:49:52 +0000

    Revert "cicd test"
    This reverts commit 533eacc03997cf4b8031987a3c9eea954ab8a27a.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-	return (
-		<nav className="navbar">
-			<Link
-				to="/"
-				className="logo"
-			>
-				Bookstore (ci/cd test)
-			</Link>
-			<div className="links">
-				<Link to="/">Home</Link>
-				<Link to="/cart">Cart</Link>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar">
+            <Link to="/" className="logo">Bookstore</Link>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/cart">Cart</Link>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
